d32acbde8a92425e4e86408fccfbf18d28dd8c97
Manash Sangam <87351102+Manash-Sangam@users.noreply.github.com>
2021-11-27 14:02:33 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function reveal()
+{
+    document.getElementById("hidden").style.display='flex';
+    document.getElementById("btn").style.display='none';
+    document.body.style.backgroundImage="url('scribble.jpg')";
+}
